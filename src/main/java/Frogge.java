49ae1e90d4e04c499bfd14c7a41ccdae60b8be51
Xiao Hui <lowxiaohui05@gmail.com>
2026-01-20 17:31:37 +0800

    Renamed bot. Added greeting.

---
+++ src/main/java/Frogge.java
@@ -0,0 +1,11 @@
+public class Frogge {
+    private static final String HORIZONTAL_LINE = "_____________________________________________";
+
+    public static void main(String[] args) {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Hello! I'm Frogge! *ribbit*");
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Goodbye! *ribbit*");
+        System.out.println(HORIZONTAL_LINE);
+    }
+}
