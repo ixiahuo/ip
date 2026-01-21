b91cc84c28351e2227e8d573b27bd538ebfcc459
Xiao Hui <lowxiaohui05@gmail.com>
2026-01-21 14:03:41 +0800

    Added classes Todo.java, Deadline.java and Event.java

---
+++ src/main/java/Event.java
@@ -0,0 +1,17 @@
+public class Event extends Task {
+    private final String start;
+    private final String end;
+
+    Event(String name, String start, String end) {
+        super(name);
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() +
+         " (from: " + this.start + 
+         " to: " + this.end + ")";
+    }
+}
