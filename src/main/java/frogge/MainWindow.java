cbecd136c03031c46c12272fd57faf22936e13f1
Xiao Hui <lowxiaohui05@gmail.com>
2026-02-02 20:54:16 +0800

    Implement a GUI

---
+++ src/main/java/frogge/MainWindow.java
@@ -0,0 +1,62 @@
+package frogge;
+
+import javafx.animation.PauseTransition;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+/**
+ * Controller for the main GUI.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Frogge frogge;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/user.png"));
+    private Image froggeImage = new Image(this.getClass().getResourceAsStream("/images/frogge.png"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    /** Injects the Frogge instance */
+    public void setFrogge(Frogge f) {
+        frogge = f;
+        dialogContainer.getChildren().add(DialogBox.getFroggeDialog(f.initGreeting(), froggeImage));
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Frogge's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = frogge.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getFroggeDialog(response, froggeImage)
+        );
+        userInput.clear();
+        if (input.equals("bye")) {
+            PauseTransition delay = new PauseTransition(Duration.seconds(1));
+            delay.setOnFinished(event -> Platform.exit());
+            delay.play();
+        }
+    }
+}
