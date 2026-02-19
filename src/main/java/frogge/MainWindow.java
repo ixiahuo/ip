1253ece9f00b007ffac49f6d319fdddf0dfc3e98
Xiao Hui <lowxiaohui05@gmail.com>
2026-02-19 14:34:09 +0800

    Change colours and add a background image for the app

--- src/main/java/frogge/MainWindow.java
+++ src/main/java/frogge/MainWindow.java
@@ -4,12 +4,14 @@ import javafx.animation.PauseTransition;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.util.Duration;
+
 
 /**
  * Controller for the main GUI.
